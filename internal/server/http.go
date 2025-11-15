7840abcf38622b868b093c5490753fee02e7755c
eren <unkmonster@outlook.com>
2025-11-16 01:08:36 +0800

    use RealIP middleware only if realip config block exists

--- internal/server/http.go
+++ internal/server/http.go
@@ -8,10 +8,11 @@ import (
 	"github.com/go-kratos/kratos-layout/internal/service"
 	"github.com/gorilla/handlers"
 	"github.com/unkmonster/go-kit/middleware/http/realip"
-	"github.com/unkmonster/go-kit/middleware/http/requestid"
 
 	"github.com/go-kratos/kratos/contrib/middleware/validate/v2"
+	"github.com/go-kratos/kratos/v2/errors"
 	"github.com/go-kratos/kratos/v2/log"
+	"github.com/go-kratos/kratos/v2/middleware"
 	"github.com/go-kratos/kratos/v2/middleware/logging"
 	"github.com/go-kratos/kratos/v2/middleware/metadata"
 	"github.com/go-kratos/kratos/v2/middleware/recovery"
@@ -35,26 +36,42 @@ func newCorsHandler(c *conf.Server_HTTP_CORS) func(http.Handler) http.Handler {
 	)
 }
 
+// hideInternalErrorEncoder 如果 err 不为 kratos error 响应 500 INTERNAL_SERVER_ERROR 给客户端
+func hideInternalErrorEncoder(w http.ResponseWriter, r *http.Request, err error) {
+	var ke *errors.Error
+	if !errors.As(err, &ke) {
+		ke = errors.InternalServer(
+			"INTERNAL_SERVER_ERROR",
+			"internal server error",
+		).WithCause(err)
+	}
+	khttp.DefaultErrorEncoder(w, r, ke)
+}
+
 // NewHTTPServer new an HTTP server.
 func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.Logger) *khttp.Server {
+	var middlewares = []middleware.Middleware{
+		recovery.Recovery(),
+		tracing.Server(),
+		metadata.Server(),
+		logging.Server(logger),
+	}
+	if c.Http.RealIp != nil {
+		middlewares = append(middlewares, realip.Server(
+			logger,
+			realip.WithIpHeaders(c.Http.RealIp.IpHeaders),
+			realip.WithTrustedHeader(c.Http.RealIp.TrustedHeader),
+			realip.WithTrustedProxies(c.Http.RealIp.TrustedProxies),
+		))
+	}
+	middlewares = append(middlewares, validate.ProtoValidate())
+
 	var opts = []khttp.ServerOption{
-		khttp.Middleware(
-			recovery.Recovery(),
-			tracing.Server(),
-			requestid.Server(),
-			realip.Server(
-				logger,
-				realip.WithIpHeaders(c.Http.RealIp.IpHeaders),
-				realip.WithTrustedHeader(c.Http.RealIp.TrustedHeader),
-				realip.WithTrustedProxies(c.Http.RealIp.TrustedProxies),
-			),
-			metadata.Server(),
-			logging.Server(logger),
-			validate.ProtoValidate(),
-		),
+		khttp.Middleware(middlewares...),
 		khttp.Filter(
 			newCorsHandler(c.Http.Cors),
 		),
+		khttp.ErrorEncoder(hideInternalErrorEncoder),
 	}
 	if c.Http.Network != "" {
 		opts = append(opts, khttp.Network(c.Http.Network))
@@ -65,6 +82,7 @@ func NewHTTPServer(c *conf.Server, greeter *service.GreeterService, logger log.L
 	if c.Http.Timeout != nil {
 		opts = append(opts, khttp.Timeout(c.Http.Timeout.AsDuration()))
 	}
+
 	srv := khttp.NewServer(opts...)
 	v1.RegisterGreeterHTTPServer(srv, greeter)
 	return srv
